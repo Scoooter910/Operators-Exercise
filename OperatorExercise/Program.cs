962574e12c5a81f18bd273159e825f953ff99c50
Michele Mostoller <MMostoller@att.net>
2024-06-04 14:05:25 -0400

    Operators Excercise 1&2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            var sum = a + b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {AreaOfCircle(radius)}");
+
+            Console.WriteLine($"{a}/{b} is {Divide(a, b)} remainder {Modulus(a, b)}");
         }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiply(int num1, int num2)
+        { return num1 * num2; }
+
+        public static int Divide(int num1, int num2)
+        { return num1 / num2; }
+
+        public static int Modulus(int num1, int num2)
+        { return num1 % num2; }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+
+        }
+
     }
+        
 }
